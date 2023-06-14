dd61137efc186ff248e942fba914a6d7d42a05f6
Pablo Piazza <ppiazza@fi.uba.ar>
2023-06-13 23:09:53 -0300

    Entregable de clase 2 - ProductManager

--- ProductManager.js
+++ ProductManager.js
@@ -7,7 +7,7 @@ class ProductManager {
     addProduct(title, description, price, thumbnail, code, stock) {
         ProductManager.lastId++;
         if(this.products.some(product => product.code=== code)) {
-            console.log("El código de producto ingresado ya existe");
+            throw new Error("El código de producto ingresado ya existe");
         }else{
         const product ={
             title,
@@ -30,21 +30,7 @@ class ProductManager {
     if(prod){
         return prod;
     }else{
-        console.log("El producto solicitado no existe")
+        throw new Error("El producto solicitado no existe");
     }
     }
 }
-
-console.log("\nNOTA: no se utilizó la sentencia 'throw new Error' para que la respuesta sea mas legible \n")
-const productoPrueba = new ProductManager();
-console.log("NOTA: Obtengo productos de lista vacía \n")
-console.log(productoPrueba.getProducts());
-productoPrueba.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin Imagen","abc123",25 );
-console.log("\nNOTA: Obtengo productos habiendo agregado un objeto \n")
-console.log(productoPrueba.getProducts());
-console.log("\nNOTA: Agrego objeto repetido por lo que obtengo mensaje de error \n")
-productoPrueba.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin Imagen","abc123",25 );
-console.log("\nNOTA: Obtengo producto con id = 1 \n")
-console.log(productoPrueba.getProductById(1));
-console.log("\nNOTA: Obtengo mensaje de error porque ID=2 no existe \n")
-console.log(productoPrueba.getProductById(2));
